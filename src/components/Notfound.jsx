72613bb3cd4c79eba2c88ec06bfb98148297cfc6
kartikeya <kartikeya.work@gmail.com>
2021-05-30 23:10:16 +0530

    finished backend

--- src/components/Notfound.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const Notfound = (props) => {
-  return <h1>Not Found</h1>;
-};
-
-export default Notfound;
--- src/components/Rental.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const Rental = () => {
-  return <h1>rental</h1>;
-};
-
-export default Rental;
